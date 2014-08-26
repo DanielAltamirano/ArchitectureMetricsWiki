74049991405c90863fdbda70c628c23b98f07c62
Daniel Altamirano <daniel.altamirano@glafon.com>
2014-08-26 19:18:17 -0300

    ArchitecturalMetrics Solution - WIP

---
+++ ArchitecturalMetrics/Models/Metric.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="Content/js/underscore.js" />
+/// <reference path="Content/js/backbone.js" />
+
+var Metric = Backbone.Model.extend({
+    defaults: {
+        MetricName: "Metric",
+        Percentage: ""
+    }
+});
