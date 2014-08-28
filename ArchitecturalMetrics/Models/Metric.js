5c00df36a180833f50ff700e4705b8af88eb482b
Daniel Altamirano <daniel.altamirano@glafon.com>
2014-08-27 22:01:43 -0300

    First views and models

--- ArchitecturalMetrics/Models/Metric.js
+++ ArchitecturalMetrics/Models/Metric.js
@@ -1,9 +1,54 @@
 ﻿/// <reference path="Content/js/underscore.js" />
 /// <reference path="Content/js/backbone.js" />
 
-var Metric = Backbone.Model.extend({
+var app = app || {};
+
+app.Metric = Backbone.Model.extend({
     defaults: {
         MetricName: "Metric",
-        Percentage: ""
+        Percentage: "0"
     }
 });
+
+app.MetricCollection = Backbone.Collection.extend({
+    model: app.Metric
+});
+
+var ADC = new app.Metric({
+    MetricName: "Architectonic Documentation Quality",
+    Percentage: 40
+});
+
+var DA = new app.Metric({
+    MetricName: "Detailed Architecture",
+    Percentage: 80
+});
+
+var DAS = new app.Metric({
+    MetricName: "Detailed Architectonic Styles",
+    Percentage: 60
+});
+
+var FR = new app.Metric({
+    MetricName: "Functional Requirements",
+    Percentage: 10
+});
+
+var PQS = new app.Metric({
+    MetricName: "Prioritized Quality Scenarios",
+    Percentage: 30
+});
+
+var TQSM = new app.Metric({
+    MetricName: "Tactics to Quality Scenarios Mapping",
+    Percentage: 55
+});
+
+var DSS = new app.Metric({
+    MetricName: "Detailed Scenario Specification",
+    Percentage: 100
+});
+
+app.metricGroup = new app.MetricCollection([
+    ADC, DA, DAS, FR, PQS, TQSM, DSS
+]);
---
+++ ArchitecturalMetrics/Views/MetricView.js
@@ -0,0 +1,32 @@
+﻿var app = app || {};
+
+app.MetricView = Backbone.View.extend({
+    tagName: "div",
+    className: "panel-body",
+    template: _.template($("#metricElement").html()),
+
+    render: function() {
+        var metricTemplate = this.template(this.model.toJSON());
+        this.$el.html(metricTemplate);
+        return this;
+    }
+});
+
+app.AllMetricsView = Backbone.View.extend({
+    tagName: "section",
+
+    render: function() {
+        this.collection.each(this.addMetric, this);
+        return this;
+    },
+
+    addMetric: function(metric) {
+        var metricView = new app.MetricView({ model: metric });
+        this.$el.append(metricView.render().el);
+    }
+});
+
+var metricsAllView = new app.AllMetricsView({collection: app.metricGroup});
+
+console.log(app.metricGroup.toJSON());
+$("#app").html(metricsAllView.render().el);
